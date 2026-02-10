0171be8d99c448f815abf259f870aa1cbf3a6c2b
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-02-10 15:48:00 +0700

    paelok

--- src/whatsappClient.js
+++ src/whatsappClient.js
@@ -6,6 +6,10 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
