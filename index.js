a9d7da43341530a0f0c7fe7eed6012ffaab75dae
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-02-10 15:41:04 +0700

    coba

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const bodyParser = require('body-parser');
 const client = require('./src/whatsappClient');
 const apiRoutes = require('./src/apiRoutes');
 
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+    res.send('WhatsApp API Gateway is running');
+});
 
 console.log('Initializing WhatsApp Client...');
 client.initialize();
