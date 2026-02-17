34ab0a44fcc83093f321a22b58ed4fff5a0305ad
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-02-17 19:44:59 +0700

    Make port configurable via PORT environment variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./src/apiRoutes');
 
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
