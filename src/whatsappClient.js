54ceb77f01ae828185405bfc518ee2f134efdce5
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-02-17 19:28:38 +0700

    Untrack session data and update client config

--- src/whatsappClient.js
+++ src/whatsappClient.js
@@ -5,7 +5,15 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
     }
 });
 
