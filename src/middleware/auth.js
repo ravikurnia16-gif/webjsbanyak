c4c3bc1197d2692ce554585a2559562b845a3689
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-03-26 16:58:13 +0700

    feat: support apikey in POST body

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const logger = require('../utils/logger');
 
 const authenticate = (req, res, next) => {
-    const apiKey = req.headers['x-api-key'] || req.query.apikey;
+    const apiKey = req.headers['x-api-key'] || req.query.apikey || req.body?.apikey;
 
     if (!apiKey || apiKey !== config.apiKey) {
         logger.warn(`Unauthorized access attempt from ${req.ip}`);
